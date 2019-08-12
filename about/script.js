7f74fdcf03f569c24dfae30d22fc7b1ca4e960f5
Ayush Suresh <byteslayer31037@gmail.com>
2019-08-12 13:01:10 -0500

    updated gitignore to hide keys and stuff

--- about/script.js
+++ about/script.js
@@ -1,19 +1,18 @@
 var database = firebase.database();
-function readDataSnapshot() {
-	return database.ref('/posts').once('value').then(function(snapshot) {
-		var testNumber = snapshot.val().test;
-		console.log(testNumber);
-	});
-}
+var list = document.getElementById('post_list_cardview');
 
-function attemptAtReadingAllChildren() {
+function readingAllChildren() {
 	return database.ref('/posts').once('value').then(function(snapshot) {
 		snapshot.forEach(function(childSnapshot) {
 			console.log(childSnapshot.val().title + "\n" + childSnapshot.val().description + "\n" + childSnapshot.val().content);
+			var entry = document.createElement('li');
+			entry.appendChild(document.createTextNode(childSnapshot.val().title));
+			entry.appendChild(document.createTextNode(childSnapshot.val().description));
+			document.getElementById('post_list_cardview').appendChild(entry);
 		});
 	});
 }
 
+readingAllChildren();
 
-readDataSnapshot();
-attemptAtReadingAllChildren();
+// NEED TO CREATE SOME SORT OF CARD VIEW TO DISPLAY POSTS!!!
