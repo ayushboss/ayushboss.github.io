f39de65b4a3aeabb0657e99255f93874bfe13100
Ayush Suresh <byteslayer31037@gmail.com>
2019-08-12 14:12:57 -0500

    made basic designs to the cardviews

--- about/script.js
+++ about/script.js
@@ -1,16 +1,24 @@
 var database = firebase.database();
 var list = document.getElementById('post_list_cardview');
+var recent_posts = document.getElementById('recent_posts');
 
 function readingAllChildren() {
 	return database.ref('/posts').once('value').then(function(snapshot) {
 		snapshot.forEach(function(childSnapshot) {
+			var card = document.createElement('div');
+
 			console.log(childSnapshot.val().title + "\n" + childSnapshot.val().description + "\n" + childSnapshot.val().content);
-			var entry = document.createElement('li');
-			var subEntry = document.createElement('li');
+			var entry = document.createElement('p');
+			var subEntry = document.createElement('p');
 			entry.appendChild(document.createTextNode(childSnapshot.val().title));
 			subEntry.appendChild(document.createTextNode('\n' + childSnapshot.val().description));
-			entry.appendChild(subEntry);
-			list.appendChild(entry);
+			card.appendChild(entry);
+			card.appendChild(subEntry);
+
+			card.style.border = "thick solid #FFFFFF"
+
+			recent_posts.appendChild(card);
+			recent_posts.appendChild(document.createElement('hr'))
 		});
 	});
 }
