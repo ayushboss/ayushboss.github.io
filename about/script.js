d355968f323b274e092302a815da3a2c941aa9f1
Ayush Suresh <byteslayer31037@gmail.com>
2019-08-13 11:32:21 -0500

    added link functionality and removed text-decoration from blog posts

--- about/script.js
+++ about/script.js
@@ -5,20 +5,34 @@ var recent_posts = document.getElementById('recent_posts');
 function readingAllChildren() {
 	return database.ref('/posts').once('value').then(function(snapshot) {
 		snapshot.forEach(function(childSnapshot) {
+			var link = document.createElement('a');
+			link.href= "../posts/" + childSnapshot.key + ".html";
 			var card = document.createElement('div');
 
-			console.log(childSnapshot.val().title + "\n" + childSnapshot.val().description + "\n" + childSnapshot.val().content);
+			var rDate = childSnapshot.val().date;
+			
 			var entry = document.createElement('p');
 			var subEntry = document.createElement('p');
+			var dateEntry = document.createElement('p');
+
 			entry.appendChild(document.createTextNode(childSnapshot.val().title));
 			subEntry.appendChild(document.createTextNode('\n' + childSnapshot.val().description));
+			dateEntry.appendChild(document.createTextNode('\n' + rDate));
+
+			dateEntry.style.cssFloat = "right";
+
+			card.appendChild(dateEntry);
 			card.appendChild(entry);
 			card.appendChild(subEntry);
+			card.appendChild(document.createElement('hr'));
 
-			card.style.border = "thick solid #FFFFFF"
+			link.appendChild(card);
+			//link.appendChild(document.createElement('hr'));
 
-			recent_posts.appendChild(card);
-			recent_posts.appendChild(document.createElement('hr'))
+			link.style.textDecoration = "none";
+			link.style.color = "white";
+
+			recent_posts.appendChild(link);
 		});
 	});
 }
