4d209b9cc0cda9d1fe485d6ba27ba7687ca40715
Ayush Suresh <byteslayer31037@gmail.com>
2019-08-17 14:27:41 -0500

    added a tag aspect to cards and made cards more equally spaced

--- about/script.js
+++ about/script.js
@@ -12,19 +12,31 @@ function readingAllChildren() {
 			var rDate = childSnapshot.val().date;
 			
 			var entry = document.createElement('h2');
+			var tag = document.createElement('h4');
 			var subEntry = document.createElement('p');
 			var dateEntry = document.createElement('p');
 
 			entry.appendChild(document.createTextNode(childSnapshot.val().title));
 			subEntry.appendChild(document.createTextNode('\n' + childSnapshot.val().description));
 			dateEntry.appendChild(document.createTextNode('\n' + rDate));
+			tag.appendChild(document.createTextNode('\n' + "Bosnia"));
 
+			entry.style.paddingLeft = "10px";
+			entry.style.paddingTop = "10px";
+			tag.style.paddingLeft = "10px";
+			subEntry.style.paddingLeft = "10px";
+			dateEntry.style.paddingRight = "10px";
+
+
+			tag.style.color = "grey";
 			dateEntry.style.cssFloat = "right";
 
 			card.appendChild(dateEntry);
 			card.appendChild(entry);
+			card.appendChild(tag);
 			card.appendChild(subEntry);
 			card.appendChild(document.createElement('hr'));
+			//card.style.border = "thin dashed white";
 
 			link.appendChild(card);
 			//link.appendChild(document.createElement('hr'));
