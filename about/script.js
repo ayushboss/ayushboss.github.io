c53192662cb7a65478feea9b786919d6db50ef89
Ayush Suresh <byteslayer31037@gmail.com>
2019-08-17 16:49:04 -0500

    added tags and changed coloring of links

--- about/script.js
+++ about/script.js
@@ -6,40 +6,62 @@ function readingAllChildren() {
 	return database.ref('/posts').once('value').then(function(snapshot) {
 		snapshot.forEach(function(childSnapshot) {
 			var link = document.createElement('a');
-			link.href= "../blog/posts/" + childSnapshot.key + ".html";
+			//link.href= "../blog/posts/" + childSnapshot.key + ".html";
 			var card = document.createElement('div');
 
 			var rDate = childSnapshot.val().date;
 			
-			var entry = document.createElement('h2');
-			var tag = document.createElement('h4');
+			var entry = document.createElement('a');
+
+			entry.style.fontSize = "24px";
+			entry.href="../blog/posts/" + childSnapshot.key + ".html";
+
+			var tag = document.createElement('a');
 			var subEntry = document.createElement('p');
 			var dateEntry = document.createElement('p');
+			var tagC = document.createElement('div');
 
 			entry.appendChild(document.createTextNode(childSnapshot.val().title));
 			subEntry.appendChild(document.createTextNode('\n' + childSnapshot.val().description));
 			dateEntry.appendChild(document.createTextNode('\n' + rDate));
 			tag.appendChild(document.createTextNode('\n' + "Bosnia"));
 
-			entry.style.paddingLeft = "10px";
+			entry.style.color = "#7cafc2";
+
 			entry.style.paddingTop = "10px";
 			tag.style.paddingLeft = "10px";
-			subEntry.style.paddingLeft = "10px";
+			//subEntry.style.paddingLeft = "10px";
+			subEntry.style.paddingTop = "5px";
 			dateEntry.style.paddingRight = "10px";
 
-
 			tag.style.color = "grey";
+
+			tag.href = "https://cnn.com";
+			tag.style.textDecoration = "none";
+
+			tagC.style.backgroundColor = "#282828";
+			tagC.style.borderRadius = "5px";
+			//tagC.style.padding = "2px 6px";
+			//tagC.style.margin = "0 4px";
+			tagC.style.width = "70px";
+
+			tagC.appendChild(tag);
+
 			dateEntry.style.cssFloat = "right";
 
 			card.appendChild(dateEntry);
 			card.appendChild(entry);
-			card.appendChild(tag);
+			card.appendChild(tagC);
 			card.appendChild(subEntry);
-			card.appendChild(document.createElement('hr'));
+			//card.appendChild(document.createElement('hr'));
 			//card.style.border = "thin dashed white";
 
 			link.appendChild(card);
-			//link.appendChild(document.createElement('hr'));
+
+			var h = document.createElement('hr');
+			h.style.color = "white";
+
+			link.appendChild(h);
 
 			link.style.textDecoration = "none";
 			link.style.color = "white";
