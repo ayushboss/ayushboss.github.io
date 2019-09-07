aafddc46ced54e0b5f009349c08492bc666000ae
Ayush Suresh <byteslayer31037@gmail.com>
2019-09-07 20:15:06 +0200

    gitignore2

--- config.js
+++
@@ -1,9 +0,0 @@
-var config = {
-	API_KEY: "AIzaSyA0lvcWlvmnl9ifXnwH-Y9A4f0h3EiE-9Q",
-	AUTH_DOMAIN: "websiteandportfolio.firebaseapp.com",
-	DATABASE_URL: "https://websiteandportfolio.firebaseio.com",
-	PROJECT_ID: "websiteandportfolio",
-	STORAGE_BUCKET: "",
-	MESSAGE_SENDER_ID: "406560027917",
-	APP_ID: "1:406560027917:web:60e80ab54537dc85"
-}
